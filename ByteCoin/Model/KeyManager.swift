46fd1432661cf624098765ed9fa8326576864d9f
Avneet Sekhon <avneetsekhonfp@gmail.com>
2023-06-26 16:12:20 -0700

    API key integration 1.4

---
+++ ByteCoin/Model/KeyManager.swift
@@ -0,0 +1,13 @@
+//
+//  KeyManager.swift
+//  ByteCoin
+//
+//  Created by Avneet Sekhon on 2023-06-26.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct KeyManager {
+    let coinAPI = "tes1143t"
+}
