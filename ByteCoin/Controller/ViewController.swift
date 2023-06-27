633927e42b61551e9ca9fa91055eee8e8694b7a3
Avneet Sekhon <avneetsekhonfp@gmail.com>
2023-06-27 12:49:54 -0700

    Refactor with Extensions

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 //Important to adpot CoinManagerDelegate Protocol
-class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate, CoinManagerDelegate {
+class ViewController: UIViewController {
     
     var coinManager = CoinManager() //var because it allows the properties to change
 
@@ -20,14 +20,23 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        coinManager.delegate = self //Set the coinManager's delegate as current class to receive notification from delagate methods that are called
+        coinManager.delegate = self //Set the coinManager's delegate to current class to receive notifications from delagate methods that are called
         bitcoinPicker.dataSource = self //ViewController is the datasource for the picker
         bitcoinPicker.delegate = self
         
     }
-    
+}
+
+
+//MARK: - CoinManagerDelegate
+
+extension ViewController: CoinManagerDelegate {
     func didUpdatePrice(_ coinManager: CoinManager, price: String, currency: String) {
-        DispatchQueue.main.async {      //Dispatch.. gets a hold of the main thread
+        
+        /*
+         DispatchQueue gets a hold of the main thread. Data is coming from a networking     completion handler which usually runs in the background and can take a few seconds to minutes to complete. Therefore UI updates need to be done on the main thread.
+         */
+        DispatchQueue.main.async {
             self.bitcoinLabel.text = price
             self.bitcoinCurrency.text = currency
         }
@@ -36,19 +45,28 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func didFailWithError(error: Error) {
         print(error)
     }
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1    //Number of columns in picker
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return coinManager.currencyArray.count  //Number of rows per column
-    }
+}
 
+//MARK: - UIPickerViewDelegate
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    //Number of columns in picker
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    //Number of rows per column based on array size
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return coinManager.currencyArray.count
+    }
+    
+    //Row titles from the array
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return coinManager.currencyArray[row]
     }
     
+    //Sends the selected currency
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         coinManager.getCoinPrice(for: coinManager.currencyArray[row])
     }
